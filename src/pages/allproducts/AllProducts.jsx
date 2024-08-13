6a7c9127b810593675a6ccfd6c02e49b62164e78
hemraj <hemrajsaini8078@gmail.com>
2024-08-13 16:51:06 +0530

    changes

--- src/pages/allproducts/AllProducts.jsx
+++ src/pages/allproducts/AllProducts.jsx
@@ -45,13 +45,13 @@ function Allproducts() {
                         const { title, price, description, imageUrl,id } = item;
                         return (
                             <div onClick={()=> window.location.href = `/productinfo/${id}`}   key={index} className="p-4 md:w-1/4  drop-shadow-lg " >
-                                <div className="h-full border-2 hover:shadow-gray-100 hover:shadow-2xl transition-shadow duration-300 ease-in-out sm:ml-[80px]  border-gray-200 border-opacity-60 rounded-2xl overflow-hidden sm:w-full" style={{ backgroundColor: mode === 'dark' ? 'rgb(46 49 55)' : '', color: mode === 'dark' ? 'white' : '', }} >
-                                    <div className="flex justify-center cursor-pointer sm:w-[200%]" >
-                                        <img className=" rounded-2xl w-full h-80 p-2 md:w-full sm:border border-b-amber-800  hover:scale-110 transition-scale-110 duration-300 ease-in-out" src={imageUrl} alt="blog" />
-                                    </div>
+                                <div className="h-full border-2 hover:shadow-gray-100 hover:shadow-2xl transition-shadow duration-300 ease-in-out   border-gray-200 border-opacity-60 rounded-2xl overflow-hidden " style={{ backgroundColor: mode === 'dark' ? 'rgb(46 49 55)' : '', color: mode === 'dark' ? 'white' : '', }} >
+                                <div className="flex justify-center cursor-pointer " >
+                                <img className=" rounded-2xl w-full h-80 p-2 md:w-full  sm:border border-b-amber-800  hover:scale-110 transition-scale-110 duration-300 ease-in-out" src={imageUrl} alt="blog" />
+                            </div>
                                     <div className="p-5 border-t-2">
                                         <h2 className="tracking-widest text-xs title-font font-medium text-gray-400 mb-1" style={{ color: mode === 'dark' ? 'white' : '', }}>E-Bharat</h2>
-                                        <h1 className="title-font text-lg font-medium sm:w-[100%] text-gray-900 mb-3" style={{ color: mode === 'dark' ? 'white' : '', }}>{title}</h1>
+                                        <h1 className="title-font text-lg font-medium  text-gray-900 mb-3" style={{ color: mode === 'dark' ? 'white' : '', }}>{title}</h1>
                                         {/* <p className="leading-relaxed mb-3">{item.description.}</p> */}
                                         <p className="leading-relaxed mb-3" style={{ color: mode === 'dark' ? 'white' : '' }}>₹{price}</p>
                                         <div className=" flex justify-center">
